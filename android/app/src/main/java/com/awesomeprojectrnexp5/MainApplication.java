c90db8e26ff14ecd4ed4febacb2eceb1eea507f5
Walaa <walaa@yamsafer.me>
2018-09-19 16:49:45 +0300

    Linking - YA RB

--- android/app/src/main/java/com/awesomeprojectrnexp5/MainApplication.java
+++ android/app/src/main/java/com/awesomeprojectrnexp5/MainApplication.java
@@ -3,6 +3,9 @@ package com.awesomeprojectrnexp5;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.auth0.react.A0Auth0Package;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativeRestartPackage(),
+            new ReactNativeI18n(),
+            new A0Auth0Package()
       );
     }
 
